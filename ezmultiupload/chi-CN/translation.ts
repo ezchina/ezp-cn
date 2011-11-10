6950132a704cceb51415c7319a34b6b110945fb0
rivsen <sensen.duan@zerustech.com>
2011-11-10 09:58:11 +0800

    ezmultiupload translation

--- ezmultiupload/chi-CN/translation.ts
+++ ezmultiupload/chi-CN/translation.ts
@@ -5,54 +5,54 @@
     <name>design/admin/popupmenu</name>
     <message>
         <source>Upload multiple files</source>
-        <translation type="unfinished"></translation>
+        <translation type="">上传多个文件</translation>
     </message>
 </context>
 <context>
     <name>extension/ezmultiupload</name>
     <message>
         <source>Multiupload</source>
-        <translation type="unfinished"></translation>
+        <translation type="">多文件上传</translation>
     </message>
     <message>
         <source>The files are uploaded to</source>
-        <translation type="unfinished"></translation>
+        <translation type="">文件上传到</translation>
     </message>
     <message>
         <source>Select files</source>
-        <translation type="unfinished"></translation>
+        <translation type="">选择文件</translation>
     </message>
     <message>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation type="">错误</translation>
     </message>
     <message>
         <source>Thumbnail created.</source>
-        <translation type="unfinished"></translation>
+        <translation type="">缩略图已创建。</translation>
     </message>
     <message>
         <source>All files received.</source>
-        <translation type="unfinished"></translation>
+        <translation type="">所有文件已收到。</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation type="">取消</translation>
     </message>
     <message>
         <source>Upload canceled.</source>
-        <translation type="unfinished"></translation>
+        <translation type="">上传已取消。</translation>
     </message>
     <message>
         <source>Allowed Files</source>
-        <translation type="unfinished"></translation>
+        <translation type="">支持的文件</translation>
     </message>
     <message>
         <source>Could not load flash(or not loaded yet), this is needed for multiupload!</source>
-        <translation type="unfinished"></translation>
+        <translation type="">无法加载flash（或者还尚未加载），多文件上传需要加载flash！</translation>
     </message>
     <message>
         <source>Javascript has been disabled, this is needed for multiupload!</source>
-        <translation type="unfinished"></translation>
+        <translation type="">Javascript已经被禁用，多文件上传需要Javascript！</translation>
     </message>
 </context>
 </TS>
