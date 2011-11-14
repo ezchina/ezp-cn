49f26c0c29e2ce01b606e1d292b17dcba14fe200
Michael Lee <michael.lee@zerustech.com>
2011-11-14 12:18:35 +0800

    Add ezgmaplocation translation

--- ezgmaplocation/chi-CN/translation.ts
+++ ezgmaplocation/chi-CN/translation.ts
@@ -5,52 +5,52 @@
     <name>extension/ezgmaplocation/datatype</name>
     <message>
         <source>Latitude</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">纬度</translation>
     </message>
     <message>
         <source>Longitude</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">经度</translation>
     </message>
     <message>
         <source>Restore</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">还原</translation>
     </message>
     <message>
         <source>Find address</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">查找地址</translation>
     </message>
     <message>
         <source>Address</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">地址</translation>
     </message>
     <message>
         <source>Map</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">地图</translation>
     </message>
     <message>
         <source>My current location</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">我目前的位置</translation>
     </message>
     <message>
         <source>Restores location and address values to what it was on page load.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">将位置与地址信息恢复为初始值。</translation>
     </message>
     <message>
         <source>Gets your current position if your browser support GeoLocation and you grant this website access to it! Most accurate if you have a built in gps in your Internet device! Also note that you might still have to type in address manually!</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">得到您的当前位置，如果您的浏览器支持地理定位并且您授权本网站访问它！如果您的上网设备有内置的GPS，那么结果将是最准确的！另外请注意，您可能仍然需要手动输入地址！</translation>
     </message>
     <message>
         <source>GMap Location</source>
         <comment>Datatype name</comment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">GMap 位置</translation>
     </message>
     <message>
         <source>Missing Latitude/Longitude input.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">缺少纬度/经度输入。</translation>
     </message>
     <message>
         <source>Invalid Latitude/Longitude input.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">无效的纬度/经度输入。</translation>
     </message>
 </context>
 </TS>
