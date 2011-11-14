ed9016a6a40ba65acdcc632c2219c6e0ccd8fdbc
Michael Lee <michael.lee@zerustech.com>
2011-11-14 12:18:56 +0800

    Add ezstarrating translation

--- ezstarrating/chi-CN/translation.ts
+++ ezstarrating/chi-CN/translation.ts
@@ -6,65 +6,65 @@
     <message>
         <source>Star Rating</source>
         <comment>Datatype name</comment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">星级</translation>
     </message>
     <message>
         <source>Currently %current_rating out of 5 Stars.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">目前%current_rating颗星(最高5颗星)</translation>
     </message>
     <message>
         <source>Rate %rating stars out of 5</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">%rating颗星(最高5颗星)</translation>
     </message>
     <message>
         <source>Rating: %current_rating/5</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">评价：%current_rating/5</translation>
     </message>
     <message>
         <source>%rating_count votes cast</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">%rating_count 票</translation>
     </message>
     <message>
         <source>Thank you for rating!</source>
         <comment>When rating</comment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">感谢您的评价!</translation>
     </message>
     <message>
         <source>You have already rated this page, you can only rate it once!</source>
         <comment>When rating</comment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">您已经评价这个网页，你只能评一次分！</translation>
     </message>
     <message>
         <source>%login_link_startLog in%login_link_end or %create_link_startcreate a user account%create_link_end to rate this page.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">login_link_start登录%login_link_end%或%create_link_start注册一个用户账户%create_link_end来为这个网页评分。</translation>
     </message>
     <message>
         <source>%login_link_startLog in%login_link_end to rate this page.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">%login_link_start登录%login_link_end来为这个页面评分。</translation>
     </message>
     <message>
         <source>Disabled</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">禁用</translation>
     </message>
     <message>
         <source>disabled</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">禁用</translation>
     </message>
     <message>
         <source>Your rating has been changed, thanks for rating!</source>
         <comment>When rating</comment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">您的评价已经生效,感谢您的评价!</translation>
     </message>
     <message>
         <source>You don&apos;t have access to rate this page.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">您没有权限评价这个页面。</translation>
     </message>
 </context>
 <context>
     <name>kernel/content</name>
     <message>
         <source>Top Level Nodes</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">顶级节点</translation>
     </message>
 </context>
 </TS>
