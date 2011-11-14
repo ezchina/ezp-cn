c15836bd6805e9e172d0db3c784fde018f3ffdea
Michael Lee <michael.lee@zerustech.com>
2011-11-14 12:19:13 +0800

    Add ezstyleeditor translation

--- ezstyleeditor/chi-CN/translation.ts
+++ ezstyleeditor/chi-CN/translation.ts
@@ -5,106 +5,106 @@
     <name>design/standard/ezcssstyle</name>
     <message>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">增加</translation>
     </message>
     <message>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">删除</translation>
     </message>
     <message>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">保存</translation>
     </message>
     <message>
         <source>Apply</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">应用</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">取消</translation>
     </message>
     <message>
         <source>Upload</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">上传</translation>
     </message>
     <message>
         <source>Choose</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">选择</translation>
     </message>
     <message>
         <source>Restore</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">恢复</translation>
     </message>
     <message>
         <source>Create</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">创建</translation>
     </message>
     <message>
         <source>Version list</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">版本列表</translation>
     </message>
     <message>
         <source>Elements</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">元素</translation>
     </message>
     <message>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">帮助</translation>
     </message>
     <message>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">退出</translation>
     </message>
     <message>
         <source>Properties for</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">属性</translation>
     </message>
     <message>
         <source>Property list is empty.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">属性列表是空的。</translation>
     </message>
     <message>
         <source>Insert image</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">插入图片</translation>
     </message>
     <message>
         <source>Image list is empty.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">图片列表是空的。</translation>
     </message>
     <message>
         <source>Do you really want to remove selected properties?</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">你确实要删除选定的属性吗？</translation>
     </message>
     <message>
         <source>Do you really want to remove selected image?</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">你确定要删除选定的图像吗？</translation>
     </message>
     <message>
         <source>Rules for</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">规则</translation>
     </message>
     <message>
         <source>Version</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">版本</translation>
     </message>
     <message>
         <source>Created</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">创建</translation>
     </message>
     <message>
         <source>Modified</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">修改</translation>
     </message>
     <message>
         <source>Site style list</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">网站样式列表</translation>
     </message>
 </context>
 <context>
     <name>extension/ezstyleeditor</name>
     <message>
         <source>Image repository for given path / node ID does not exist. Please check ezstyleeditor.ini INI file configuration.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">给定的路径/节点ID不存在相应的图片库。请检查ezstyleeditor.ini配置文件。</translation>
     </message>
 </context>
 </TS>
